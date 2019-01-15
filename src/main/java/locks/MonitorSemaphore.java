66ce7b11f1d3eda606096c366bedf2d02154e8a9
calasius <calasius@gmail.com>
2019-01-14 21:24:12 -0300

    Corregir comentarios

--- src/main/java/locks/MonitorSemaphore.java
+++ src/main/java/locks/MonitorSemaphore.java
@@ -12,8 +12,8 @@ public class MonitorSemaphore {
         synchronized (this) {
             while(permits == 0) {
                 try {
-                    //Supongamos que ping ejecuta wait() se libera el monitor atomicamente. De esta forma en el caso de ping-pong
-                    //El thread pong puede ejecutar el metodo release sobre este semaforo para poder salir del wait()
+                    //Supongamos que ping ejecuta wait() se libera el monitor atomicamente. De esta forma el
+                    //thread pong puede ejecutar el metodo release sobre este semaforo.
                     this.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -26,7 +26,7 @@ public class MonitorSemaphore {
     public void release() {
         synchronized (this) {
             permits++;
-            //Supongamos el thread ping se encuentra esperando en wait(), entonces el thread pong puede hacer realese
+            //El thread pong puede hacer realese
             //Cuando se ejecuta notify() se despierta algunos de los thread esperando en wait() en este caso al thread ping.
             this.notify();
         }
