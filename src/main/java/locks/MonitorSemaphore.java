9a6d3da40e56fcf641490cd083549594dc0b4fec
calasius <calasius@gmail.com>
2019-01-14 21:20:42 -0300

    Agregar comentarios a la implementacion de Semaforo con monitores

--- src/main/java/locks/MonitorSemaphore.java
+++ src/main/java/locks/MonitorSemaphore.java
@@ -8,9 +8,12 @@ public class MonitorSemaphore {
     }
 
     public void acquire() {
+        //Monitor
         synchronized (this) {
             while(permits == 0) {
                 try {
+                    //Supongamos que ping ejecuta wait() se libera el monitor atomicamente. De esta forma en el caso de ping-pong
+                    //El thread pong puede ejecutar el metodo release sobre este semaforo para poder salir del wait()
                     this.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -23,6 +26,8 @@ public class MonitorSemaphore {
     public void release() {
         synchronized (this) {
             permits++;
+            //Supongamos el thread ping se encuentra esperando en wait(), entonces el thread pong puede hacer realese
+            //Cuando se ejecuta notify() se despierta algunos de los thread esperando en wait() en este caso al thread ping.
             this.notify();
         }
     }
--- src/main/java/problemas/PingPong.java
+++ src/main/java/problemas/PingPong.java
@@ -3,10 +3,7 @@ package problemas;
 import locks.MonitorSemaphore;
 import utils.ConcurrentUtils;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 
 public class PingPong {
 
@@ -79,37 +76,5 @@ public class PingPong {
         thread1.join();
         thread2.join();
 
-    }
-
-    private static void threadPoolSolution() throws InterruptedException {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-
-        Semaphore ping = new Semaphore(1);
-        Semaphore pong = new Semaphore(0);
-
-        executor.execute(() -> {
-            int count = 10;
-            while(count > 0) {
-                ping.acquireUninterruptibly();
-                System.out.println("ping");
-                ConcurrentUtils.sleep(1000);
-                pong.release(1);
-                count--;
-            }
-        });
-
-        executor.execute(() -> {
-            int count = 10;
-            while(count > 0){
-                pong.acquireUninterruptibly(1);
-                System.out.println("pong");
-                ConcurrentUtils.sleep(1000);
-                ping.release(1);
-                count--;
-            }
-        });
-
-        executor.shutdown();
-        executor.awaitTermination(10, TimeUnit.MINUTES);
     }
 }
